0921980157cb54cb5e4ade6361b888caa808d83e
josem71411-gif <josem71411@gmail.com>
2026-04-10 11:23:42 -0500

    Created a scanner object

--- src/main/java/com/plursight/SandwichShop.java
+++ src/main/java/com/plursight/SandwichShop.java
@@ -1,4 +1,8 @@
 package com.plursight;
-
+import java.util.Scanner;
 public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
 }
