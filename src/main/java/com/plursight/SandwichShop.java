6fa8a79d67d3cfc5831d89672073e0b6055fcc00
josem71411-gif <josem71411@gmail.com>
2026-04-10 11:25:28 -0500

    Asking for sandwich size

--- src/main/java/com/plursight/SandwichShop.java
+++ src/main/java/com/plursight/SandwichShop.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class SandwichShop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+ // Step 1: Asking for a size
+ System.out.print("enter sandwich size (1= Regular, 2 = Large): ");
+ int size = scanner.nextInt();
+
     }
 
 }
